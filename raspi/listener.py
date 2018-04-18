130c623bebb9b88a5b29206f2db158dc780e5557
Paul Timmins <paul@timmins.net>
2018-04-18 11:34:08 +0000

    added logic to log the portal and level to a file, and to fix party mode fallback

--- raspi/listener.py
+++ raspi/listener.py
@@ -9,9 +9,9 @@ from gpiozero import LED
 from time import sleep
 
 
-
-#techURL = 'http://localhost:8081/module/status/json'
+#wigwam is test server, listener.local is onsite techthulu - comment out the local to get the test server
 techURL = 'http://operation-wigwam.ingress.com:8080/v1/test-info'
+techURL = 'http://listener.local:8080/v1/info'
 pollInterval = 10
 blueout = LED(2)
 greenout = LED(3)
@@ -31,20 +31,29 @@ LED(16).on()
 
 
 
-def makeblue():
+def makeblue(level=8):
      blueout.on()
      greenout.off()
      print("portal is blue")
+     statusfile = open('/tmp/statusfile',mode="w") 
+     statusfile.write("blue"+str(level)+"\n")
+     statusfile.close()
 
-def makegreen():
+def makegreen(level=8):
      blueout.off()
      greenout.on()
      print("portal is green")
+     statusfile = open('/tmp/statusfile',mode="w")
+     statusfile.write("green"+str(level)+"\n")
+     statusfile.close()
 
-def makegray():
+def makegray(level=0):
      blueout.off()
      greenout.off()
      print("portal is gray")
+     statusfile = open('/tmp/statusfile',mode="w")
+     statusfile.write("gray\n")
+     statusfile.close()
 
 def techthulu():
     try:
@@ -52,15 +61,16 @@ def techthulu():
        jsondata = urllib.request.urlopen(techURL).read().decode('utf-8')
        portalstate = json.loads(jsondata)
        if (portalstate['result']['controllingFaction'] == "Neutral"):
-            makegray() 
+            makegray(portalstate['result']['level']) 
        if (portalstate['result']['controllingFaction'] == "Enlightened"):
-            makegreen()
+            makegreen(portalstate['result']['level'])
        if (portalstate['result']['controllingFaction'] == "Resistance"):
-            makeblue()
+            makeblue(portalstate['result']['level'])
        print("retrieval complete")
     except:
        print("retrieval from techthulu failed")
        controldata='party'
+       party()
 
 def forceblue():
      makeblue()    
@@ -74,9 +84,9 @@ def forceneutral():
 def party():
      print("party mode!")
      if (random.randint(1,2) > 1):
-         makegreen()
+         makegreen(random.randint(1,8))
      else:
-         makeblue()
+         makeblue(random.randint(1,8))
 
 
 def main():
